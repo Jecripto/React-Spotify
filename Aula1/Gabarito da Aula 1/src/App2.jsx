d91aff53d9cf0ddbd6844dc2893d856bfc731a0c
Jecripto <jeferson.araujo2@etec.sp.gov.br>
2025-02-13 20:25:01 -0300

    Treino Adicionados

---
+++ Aula1/Gabarito da Aula 1/src/App2.jsx
@@ -0,0 +1,54 @@
+//rafce (Comando para Estrutura Projeto)
+
+import React from "react";
+import Header from "./components/Header";
+
+//Arrow Function
+const App2 = () => {
+    return <div>App2</div>;
+};
+
+export default App2;
+
+//rafce
+
+//Arrow Function
+//const App2 = () => <h1>Olá, Mundo!</h1>;
+
+// Nomeação de Componente
+// PascalCase
+
+//Nomeação de Variável, Função...
+//camelCase
+
+// export default, posso importar com qualquer nome e sem chave
+// export "sem default", só posso importar através de chaves
+
+// self closing tag
+// <Header></Header>
+// <Header />
+
+// Element.style {
+//     background-color: Ant... ;
+//     padding: 20px;
+//     margin: 20px;
+//     border: solid 5px black;
+//     width:  500px
+// }
+
+// nomeação de classes em CSS
+// Metodologia BEM
+// Blocks
+// Elements
+// Modifiers
+// bloco__Elemento--modificador
+// header
+// header__link
+// header__link--small
+
+// Nomes compostos são separados por -
+
+// tag vazia no react se Chama fragment ou fragmento (faz com que eu consiga atravessar as limitações de trazer apenas um item);
+
+
+
